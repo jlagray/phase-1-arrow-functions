ff162fd089c1ff9869b9380ecf6b8e0a93fc2b2e
Jamie Grayson <126696913+jlagray@users.noreply.github.com>
2023-04-09 10:43:25 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(x, y) {
+    let z = 2000 / 100;
+    return z;
+}
+
+const square = a => {
+    return a * a;
+}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
